7f0dcc334dbda3adde95e0698bcce303c0dbcb8a
Marco Vinicio <mvglizarraga@gmail.com>
2018-06-02 12:46:30 -0500

    salu2 uwu

---
+++ script.js
@@ -0,0 +1,80 @@
+
+/*
+  String
+  Number
+  Boolean
+
+  undefined
+  null
+
+*/
+
+// console.log(22*(2+2))
+//
+// var a = 2;
+// var b = 50;
+//
+//
+// var c = a*b;
+// console.log(c);
+
+/*
+  =
+*/
+
+// var edad = 16;
+//
+// console.log(edad == 16 || (edad > 18 && edad < 30));
+
+// // declarar las variables
+// var edad1 = 15;
+// var edad2 = 16;
+// var edad3 = 25;
+//
+// // declarar la funcion
+// function comparar(x) {
+//   console.log(x > 18 && x < 35)
+// }
+//
+// // ejecutar la funcion
+// comparar(edad1);
+// comparar(edad2);
+// comparar(edad3);
+// console.log(edad4 > 18 && edad4 < 30);
+
+
+// comparar('uwu', 24);
+// console.log(comparar);
+
+// function resta(x, y) {
+//   return x-y
+// }
+//
+// function saludo(nombre) {
+//   return 'Hola me llamo ' + nombre;
+// }
+//
+// var x = resta(10, 4)
+// console.log('xd', x);
+// console.log(saludo('Marco'));
+// console.log(saludo('Josue'));
+
+// condicionales
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
